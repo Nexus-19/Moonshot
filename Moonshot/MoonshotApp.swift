08aba304ac75e5f2f00951d8cdd5b53ef142c823
Soumyattam Dey <soumyttamdey14@gmail.com>
2021-07-09 23:15:51 +0530

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Soumyattam Dey on 09/07/21.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
